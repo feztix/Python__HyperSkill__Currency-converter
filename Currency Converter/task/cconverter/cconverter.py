b4d796abcbb7db8bda0fb51ed3cc1e4ea8ee41bd
feztix <pan0v.s.s@yandex.ru>
2023-04-01 13:46:06 +0300

    Stage 4/6: Stage 4 completed

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,5 +1,7 @@
 import sys
 
+currencies_exchange = {'RUB': 2.98, 'ARS': 0.82, 'HNL': 0.17, 'AUD': 1.9622, 'MAD': 0.208}
+
 
 def main(args=None):
     """The main routine."""
@@ -9,18 +11,25 @@ def main(args=None):
 
 
 def user_input():
-    number_of_conicoins = str(input("Please, enter the number of conicoins you have: "))
-    exchange_rate = str(input("Please, enter the exchange rate: "))
+    # number_of_conicoins = str(input("Please, enter the number of conicoins you have: "))
+    number_of_conicoins = str(input())
+    # exchange_rate = str(input("Please, enter the exchange rate: "))
 
-    if ("." in number_of_conicoins) or ("." in exchange_rate):
-        return float(number_of_conicoins), float(exchange_rate)
-    else:
-        return int(number_of_conicoins), int(exchange_rate)
+    # if ("." in number_of_conicoins) or ("." in exchange_rate):
+    #     return float(number_of_conicoins), float(exchange_rate)
+    # else:
+    #     return int(number_of_conicoins), int(exchange_rate)
+    return float(number_of_conicoins)
 
 
 def converter():
-    number_of_conicoins, exchange_rate = user_input()
-    print(f'The total amount of dollars: {(number_of_conicoins * exchange_rate)}')
+    # number_of_conicoins, exchange_rate = user_input()
+    number_of_conicoins = user_input()
+    # print(f'The total amount of dollars: {(number_of_conicoins * exchange_rate)}')
+
+    for c in currencies_exchange:
+        print(
+            f'I will get {round(number_of_conicoins * currencies_exchange[c], 2)} {c} from the sale of {number_of_conicoins} conicoins.')
 
 
 if __name__ == "__main__":
